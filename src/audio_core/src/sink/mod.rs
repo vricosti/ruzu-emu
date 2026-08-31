3344b68947e05dd7307a0bfb448852b27aa619d8
Vince Ricosti <80467769+vricosti@users.noreply.github.com>
2026-08-31 22:04:50 +0200

    audio: avoid backend stop deadlock

--- src/audio_core/src/sink/mod.rs
+++ src/audio_core/src/sink/mod.rs
@@ -9,4 +9,6 @@ pub mod sink_stream;
 pub use null_sink::NullSink;
 pub use sink::{Sink, SinkBox, SinkHandle, AUTO_DEVICE_NAME};
 pub use sink_details::{create_sink_from_id, get_device_list_for_sink, get_sink_ids};
-pub use sink_stream::{SinkBuffer, SinkStream, SinkStreamHandle, StreamType};
+pub use sink_stream::{
+    start_sink_stream, stop_sink_stream, SinkBuffer, SinkStream, SinkStreamHandle, StreamType,
+};
